02adb79e31b8d90016bdd41aa5b18696ef921cd6
Aaron Trevino <Aaron@aarons-mbp.attlocal.net>
2018-07-03 22:04:14 -0500

    added animation javascript

--- js/main-page.js
+++ js/main-page.js
@@ -1,4 +1,40 @@
-$('.logo-menu').click(function () {
-    $('.logo-menu').css('ani')
+// $('.enter-button').click(function () {
+//     $( ".enter-button" ).fadeOut();
+//
+// });
 
-})
+//this function controls dots expansion
+$(function() {
+    $('#enter-button').hover(function() {
+        $('.dots-outer').css({
+            height: 300,
+            width: 300
+        });
+        $('.dots-middle').css({
+            height: 250,
+            width: 250
+        });
+        $('.dots-inner').css({
+            height: 420,
+            width: 420
+        });
+
+
+    }, function() {
+        // on mouseout, reset the background colour
+        $('.dots-outer').css({
+            height: 200,
+            width: 200
+        });
+        $('.dots-middle').css({
+            height: 200,
+            width: 200
+        });
+        $('.dots-inner').css({
+            height: 200,
+            width: 200,
+            transition: 200
+        });
+
+    });
+});
