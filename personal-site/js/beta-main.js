a90f87c1f4e34e2f78738bddaa3ccbf58d90faae
Aaron Trevino <Aaron@aarons-mbp.attlocal.net>
2018-09-13 22:26:50 -0500

    added carousel

--- personal-site/js/beta-main.js
+++ personal-site/js/beta-main.js
@@ -1,12 +1,18 @@
 //WINDOW HEIGHT FOR NAVBAR--------------------------
 $(document).ready(function () {
+
+    $('.sidenav').sidenav();
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true,
+        indicators: true
+    });
+
     let wh = $(window).height();
     $("html body").css({
         "height": wh
-
     });
-    let isScrolled = false;
-    $('.sidenav').sidenav();
+
     $("a").on('click', function (event) {
         if (this.hash !== "") {
             event.preventDefault();
@@ -23,12 +29,12 @@ $(document).ready(function () {
         // .log to determine when you want the
         // nav bar to stick.
         console.log($(window).scrollTop());
-        if ($(window).scrollTop() > (wh - 64)) {
+        if ($(window).scrollTop() > (wh - 119)) {
             $('#nav-bar').css('position', 'fixed');
             $('#nav-bar').css('top', 0);
             $('#nav-bar').css('margin', 0);
         }
-        if ($(window).scrollTop() < (wh - 64)) {
+        if ($(window).scrollTop() < (wh - 119)) {
             $('#nav-bar').css('position', 'absolute');
             $('#nav-bar').css('bottom', 0);
             $('#nav-bar').css('top', 'auto');
