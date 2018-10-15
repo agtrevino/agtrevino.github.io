eb5af38cfab837a00de85015b5cdccb4161ced5b
agtrevino <aaron.g.trevino@gmail.com>
2018-10-15 15:38:25 -0500

    updates

--- personal-site/js/beta-main.js
+++ personal-site/js/beta-main.js
@@ -49,12 +49,12 @@ $(document).ready(function () {
         // .log to determine when you want the
         // nav bar to stick.
         console.log($(window).scrollTop());
-        if ($(window).scrollTop() > (wh - 119)) {
+        if ($(window).scrollTop() > (wh - 69)) {
             $('#nav-bar').css('position', 'fixed');
             $('#nav-bar').css('top', 0);
             $('#nav-bar').css('margin', 0);
         }
-        if ($(window).scrollTop() < (wh - 119)) {
+        if ($(window).scrollTop() < (wh - 69)) {
             $('#nav-bar').css('position', 'absolute');
             $('#nav-bar').css('bottom', 0);
             $('#nav-bar').css('top', 'auto');
